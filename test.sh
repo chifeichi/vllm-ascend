e7790dd3c927245330031c8dc353c1e917a93667
chifeichi <chifeichi@tongchengxiaodeMacBook-Pro.local>
2026-08-12 19:52:51 +0800

    log

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-grep "VERL_PD_CACHE_FINISH" 1.log |
+grep "VERL_PD_CACHE_SCHED" 1.log |
 awk '
 {
   delete v
@@ -6,21 +6,36 @@ awk '
     split($i,a,"=")
     v[a[1]]=a[2]
   }
+
   n++
-  total += v["total_tokens"]
-  retained += v["retained_hit_tokens_after_free"]
-  before += v["hash_entries_before"]
-  after += v["hash_entries_after"]
-  if (v["retained_hit_tokens_after_free"] == 0) zero++
-  if (v["hash_entries_after"] < v["hash_entries_before"]) shrunk++
+  session=v["session_id"]
+
+  if (session in last_first_hash && last_first_hash[session] != v["first_hash"])
+    first_hash_changed++
+
+  if (session in last_salt && last_salt[session] != v["cache_salt"])
+    cache_salt_changed++
+
+  if (session in last_lora && last_lora[session] != v["lora_id"])
+    lora_changed++
+
+  if (session in last_mm && last_mm[session] != v["mm_features"])
+    mm_changed++
+
+  if (v["skip_prefix_read"] != "False")
+    skip_prefix_read++
+
+  last_first_hash[session]=v["first_hash"]
+  last_salt[session]=v["cache_salt"]
+  last_lora[session]=v["lora_id"]
+  last_mm[session]=v["mm_features"]
 }
 END {
   print "samples=" n
-  print "avg_total_tokens=" total/n
-  print "avg_retained_hit_tokens_after_free=" retained/n
-  print "retained_ratio=" 100*retained/total "%"
-  print "zero_retained_count=" zero
-  print "avg_hash_entries_before=" before/n
-  print "avg_hash_entries_after=" after/n
-  print "hash_entries_shrunk_count=" shrunk
+  print "sessions=" length(last_first_hash)
+  print "first_hash_changed=" first_hash_changed+0
+  print "cache_salt_changed=" cache_salt_changed+0
+  print "lora_changed=" lora_changed+0
+  print "mm_changed=" mm_changed+0
+  print "skip_prefix_read=" skip_prefix_read+0
 }'
