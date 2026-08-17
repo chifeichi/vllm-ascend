c16e851db711d9969c817eb1eae274a23b7c9f4b
chifeichi <chifeichi@tongchengxiaodeMacBook-Pro.local>
2026-08-17 15:13:01 +0800

    log

--- test.sh
+++ test.sh
@@ -1,19 +1,6 @@
-grep -a "MooncakeConnector finish req not in reqs to process" <vllm日志文件> \
+grep -a "MooncakeConnector finish req not in reqs to process" <日志文件> \
 | sed -nE 's/.*request_id=([^[:space:].,]+).*/\1/p' \
 | sort \
 | uniq -c \
-| awk '
-NR == 1 { min=$1; max=$1 }
-{
-  requests++;
-  warnings += $1;
-  if ($1 < min) min=$1;
-  if ($1 > max) max=$1
-}
-END {
-  print "warnings=" warnings;
-  print "unique_requests=" requests;
-  print "avg_per_request=" warnings / requests;
-  print "min_per_request=" min;
-  print "max_per_request=" max;
-}'
+| awk '{count[$1]++} END {for (n in count) print "warnings_per_request=" n, "requests=" count[n]}' \
+| sort -n
