f869d3935cd6057997d163abfcd756f23f00df41
chifeichi <chifeichi@tongchengxiaodeMacBook-Pro.local>
2026-09-01 11:34:08 +0800

    test log

--- test.sh
+++ test.sh
@@ -1,17 +1,1 @@
-python - <<'PY'
-import pandas as pd
-
-x = pd.read_csv("swe_rebench_pd_selection.csv")
-
-for c in [
-    "rollout_n_eligible",
-    "trajectory_eligible",
-    "tail_eligible",
-    "cache_eligible",
-    "turns_eligible",
-    "ratio_eligible",
-    "selection_eligible",
-]:
-    ok = x[c].astype(str).str.lower().eq("true")
-    print(c, ok.sum())
-PY
+python -c 'import pandas as pd; x=pd.read_csv("swe_rebench_pd_selection.csv"); print(x[["p_work_proxy_mean","model_tokens_mean","decode_pressure_proxy","total_work_proxy_mean"]].quantile([0,.25,.5,.75,1]).round(3))'
